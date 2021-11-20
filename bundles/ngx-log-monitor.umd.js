fa9cf2cfa94ff301cb22b14fae333f8a69fb9ea3
Tom Tang <tom@fake.com>
2021-11-20 15:38:53 -0500

    New publish

--- bundles/ngx-log-monitor.umd.js
+++ bundles/ngx-log-monitor.umd.js
@@ -226,8 +226,9 @@
     var normalizeLogMessage = function (msg) { return (__assign(__assign({}, msg), { type: (msg.type ? msg.type : 'LOG'), timestamp: (msg.timestamp ? msg.timestamp : new Date().toLocaleString()) })); };
 
     var LogMonitorComponent = /** @class */ (function () {
-        function LogMonitorComponent(zone) {
+        function LogMonitorComponent(zone, cd) {
             this.zone = zone;
+            this.cd = cd;
             this.delay = 500;
             this.history = [];
             this.theme = 'dark';
@@ -242,7 +243,8 @@
                 this._subr = this.logStream
                     .pipe(operators.delay(this.delay))
                     .subscribe(function (a) {
-                    _this._history.push(normalizeLogMessage(a));
+                    _this._history = __spread(_this._history, [normalizeLogMessage(a)]);
+                    _this.cd.detectChanges();
                     _this.zone.run(function () {
                         setTimeout(function () { return _this.scrollToBottom(); });
                     });
@@ -266,7 +268,8 @@
             this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
         };
         LogMonitorComponent.ctorParameters = function () { return [
-            { type: core.NgZone }
+            { type: core.NgZone },
+            { type: core.ChangeDetectorRef }
         ]; };
         __decorate([
             core.Input(),
@@ -308,9 +311,10 @@
             core.Component({
                 selector: 'log-monitor',
                 template: "\n<div *ngIf=\"title\" class=\"title-bar\" [ngClass]=\"theme+'-theme'\">&#9002; {{title}}</div>\n<div\n  #container\n  class=\"container\"\n  [ngClass]=\"theme+'-theme'\"\n>\n  <span\n    class=\"msg-item {{'msg-'+(log.type.toLowerCase())}}\"\n    *ngFor=\"let log of _history\"\n    [ngClass]=\"{'animated': animated}\"\n  >\n    <span *ngIf=\"icons\" [ngSwitch]=\"log.type\">\n      <ng-container *ngSwitchCase=\"'ERR'\">&#10008;</ng-container>\n      <ng-container *ngSwitchCase=\"'SUCCESS'\">&#10004;</ng-container>\n      <ng-container *ngSwitchCase=\"'LOG'\">&#10097;</ng-container>\n      <ng-container *ngSwitchCase=\"'WARN'\">&#8252;</ng-container>\n      <ng-container *ngSwitchCase=\"'INFO'\">&#8505;</ng-container>\n    </span>\n\n    [{{log.timestamp}}]: {{log.message}}\n  </span>\n\n</div>\n",
+                changeDetection: core.ChangeDetectionStrategy.OnPush,
                 styles: [".container{position:relative;width:100%;height:100%;padding-top:5px;padding-bottom:5px;overflow-y:auto;font-family:\"Lucida Console\",Monaco,monospace,sans-serif}.animated{-webkit-animation:260ms fadein;animation:260ms fadein}@-webkit-keyframes fadein{from{opacity:0;transform:translateY(-10px)}to{opacity:1;transform:translateY(0)}}@keyframes fadein{from{opacity:0;transform:translateY(-10px)}to{opacity:1;transform:translateY(0)}}.title-bar{padding:5px;font-weight:600}.dark-theme.title-bar{border-bottom:1px solid #3c3c3c;background:#1f1f1f}.light-theme.title-bar{border-bottom:1px solid #a8acad;background:#dbdfe0}.msg-item{display:block;margin-left:6px;font-size:.85em}.msg-list{margin:0;height:100%}.light-theme{color:#212121;background:#ecf0f1}.light-theme .msg-info{color:#2980b9}.light-theme .msg-err{color:#c0392b}.light-theme .msg-success{color:#27ae60}.light-theme .msg-warn{color:#f39c12}.dark-theme{color:#ecf0f1;background:#212121}.dark-theme .msg-info{color:#3498db}.dark-theme .msg-err{color:#e74c3c}.dark-theme .msg-success{color:#2ecc71}.dark-theme .msg-warn{color:#f1c40f}"]
             }),
-            __metadata("design:paramtypes", [core.NgZone])
+            __metadata("design:paramtypes", [core.NgZone, core.ChangeDetectorRef])
         ], LogMonitorComponent);
         return LogMonitorComponent;
     }());
--- lib/log-monitor.component.d.ts
+++ lib/log-monitor.component.d.ts
@@ -1,8 +1,9 @@
-import { AfterViewInit, ElementRef, NgZone, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, ElementRef, NgZone, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { LogMessage } from './models/log-message.model';
 import { Observable } from 'rxjs';
 export declare class LogMonitorComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit {
     private zone;
+    private cd;
     delay: number;
     title: any;
     logStream: Observable<LogMessage>;
@@ -14,7 +15,7 @@ export declare class LogMonitorComponent implements OnInit, OnDestroy, OnChanges
     container: ElementRef;
     _history: LogMessage[];
     private _subr;
-    constructor(zone: NgZone);
+    constructor(zone: NgZone, cd: ChangeDetectorRef);
     ngOnInit(): void;
     ngOnDestroy(): void;
     ngOnChanges(changes: SimpleChanges): void;
